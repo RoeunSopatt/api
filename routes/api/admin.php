4c6fb4bc2a5e4cd43e04bec1c75af6fdc4978f58
Sopatt Rouern <www.vanhong168@gmail.com>
2024-04-29 20:27:49 +0700

    Create Route Group for Product

--- routes/api/admin.php
+++ routes/api/admin.php
@@ -5,3 +5,16 @@ use Illuminate\Support\Facades\Route;
 
 //=============> Create Route Dashboard<==================//
 Route::get('/dashboard',[DashboardController::class,'getDashboardInfo']);
+
+Route::group(['prefix'=> 'products'], function () {
+
+    //=> Product
+    Route::get('/',     [ProductController::class,  'getData']);
+    Route::get('/{id}', [ProductController::class,  'view']);
+    Route::post('/',    [ProductController::class,  'create']);
+    Route::post('/{id}',[ProductController::class,  'update']);
+    Route::delete('/{id}',[ProductController::class,'delete']);
+    Route::get('/transactions/{id}', [ProductController::class,'getProduct']);
+
+    
+});

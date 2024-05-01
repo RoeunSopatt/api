a9917fd5d677e883e1c98e79f506cdfa248b2605
Sopatt Rouern <www.vanhong168@gmail.com>
2024-05-01 21:36:50 +0700

    Create function view for user

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -27,4 +27,19 @@ class UserController extends Controller
         $data = $data->orderBy('id','desc')->paginate($request->limit? $request->limit: 10,);
         return response()->json($data,Responese::HTTP_OK);
     }
+
+    public function view($id=0){
+
+        $data =User::select('id','name','phone', 'phone', 'email', 'type_id', 'avatar', 'create_at', 'is_active')->with(['type'])->find( $id );
+
+        if($data){
+            return response()->json($data,Responese::HTTP_OK);
+        }else{
+
+            return response()->json([
+                'status'=>'fail',
+                'message'=> 'រកទិន្នន័យមិនឃើញក្នុងប្រព័ន្ធ'
+            ],Responese::HTTP_OK);
+        }
+    }
 }

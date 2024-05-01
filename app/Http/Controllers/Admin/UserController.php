69b77b220ad5783b6b98619c16df8902cab0ed70
Sopatt Rouern <www.vanhong168@gmail.com>
2024-05-01 21:51:45 +0700

    Create function create for user

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -5,7 +5,9 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use App\Models\User\Type;
+use App\Services\FileUpload;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
@@ -40,6 +42,57 @@ class UserController extends Controller
                 'status'=>'fail',
                 'message'=> 'រកទិន្នន័យមិនឃើញក្នុងប្រព័ន្ធ'
             ],Responese::HTTP_OK);
+        }
+    }
+
+    public function create(Request $request){
+        $this->validate(
+            $request,
+            [
+                'type_id'   => 'required|min:1|max:20',
+                'name'      => 'required|min:1|max:20',
+                'phone'     => 'required|unique:user,phone',
+                'password'    => 'required|min:6|max:20',
+                'email'         => 'unique:user,email',
+            
+            ],
+            [
+                'name.required' => 'សូមវាយបញ្ចូលឈ្មោះរបស់អ្នក',
+                'phone.required' => 'សូមវាយបញ្ចូលលេខទូរស័ព្ទរបស់អ្នក',
+                'phone.unique' =>'លេខទូរស័ព្ទនេះត្រូវបានប្រើប្រាស់រួចហើយនៅក្នុងប្រព័ន្ធ',
+                'email.unique'=> 'អ៊ីម៉េលនេះបានប្រើរួចរាល់ហើយ',
+                'password.required'=> 'សូមវាយបញ្ចូលពាក្យសម្ងាត់របស់អ្នក',
+                'password.min'=> 'សូមបញ្ចូលលេខសម្ងាត់ធំជាងឬស្មើ៦',
+                'password.mាខ'=> 'សូមបញ្ចូលលេខសម្ងាត់តូចឬស្មើ២០',
+            ]
+        );
+
+        $user =new User;
+        $user->name = $request->name;
+        $user->type_id = $request->type_id;
+        $user->phone = $request->phone;
+        $user->email = $request->email;
+        $user->password = Hash::make($request->password);
+        $user->is_active=1;
+        $user->avatar = '';
+        if ($request->image) { // Yes
+
+            // Call to File Service
+            $image     = FileUpload::uploadFile($request->image, 'users', $request->fileName);
+
+            // Only valid url can be used.
+            if ($image['url']) {
+                $user->avatar = $image['url'];
+            }
+            $user->save();
+
+        // ===> Success Response Back to Client
+            return response()->json([
+                'user'  => User::select('id', 'name', 'phone', 'email', 'type_id', 'avatar', 'created_at', 'is_active')->with(['type'])->find($user->id),
+                'message' => 'User: ' . $user->name . ' has been successfully created.'
+            ], Response::HTTP_OK);
+
+
         }
     }
 }

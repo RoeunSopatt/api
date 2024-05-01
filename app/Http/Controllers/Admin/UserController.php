1cf8b051bc782850ddf630f78496375e3a1b3c6f
Sopatt Rouern <www.vanhong168@gmail.com>
2024-05-01 21:30:35 +0700

    Create function getData for user

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use App\Models\User\Type;
 use Illuminate\Http\Request;
 
@@ -13,6 +14,17 @@ class UserController extends Controller
 
         $data=Type::get();
 
+        return response()->json($data,Responese::HTTP_OK);
+    }
+    public function getData(Request $request){
+
+        $data = User::select('id', 'name', 'phone', 'email', 'type_id', 'avatar', 'create_at', 'is_active')
+        ->with(['type']);
+        if($request->key && $request->key != ''){
+            $data = $data->where('name','LIKE','%' . $request->key .'%')->Orwhere('phone', 'LIKE', '%' .$request->key .'%');
+
+        }
+        $data = $data->orderBy('id','desc')->paginate($request->limit? $request->limit: 10,);
         return response()->json($data,Responese::HTTP_OK);
     }
 }

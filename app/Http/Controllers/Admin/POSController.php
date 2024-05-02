b6cc1971fd982f2d5b336f021f2293a8bbbcfd7b
Sopatt Rouern <www.vanhong168@gmail.com>
2024-05-02 21:10:50 +0700

    Create function _sendNotification in pos

--- app/Http/Controllers/Admin/POSController.php
+++ app/Http/Controllers/Admin/POSController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use App\Models\Orders\Order;
 use App\Models\Products\Type;
+use App\Services\TelegramService;
 use Illuminate\Http\Request;
 use ProductType;
 use Tymon\JWTAuth\Facades\JWTAuth;
@@ -27,5 +28,33 @@ class POSController extends Controller
             return $this->_generateRecieptNumber();
         }
         return $number;
+    }
+    private function _senNotification($orderData){
+        $htmlMessage ="<b> ការបញ្ជាទិញទទួលបានជោគជ័យ!</b>\n";
+        $htmlMessage ="<b> -លេខវិកយប័ត្រ៖" .$orderData->reciept_number . "\n";
+        $htmlMessage ="<b> -អ្នកគិតលុយ->" .$orderData->cashier->name;
+
+        $productList= '';
+        $totalProducts =0;
+
+        foreach($orderData->details as $detail){
+            $productList .=sprintf(
+                "%-20s | %-15s | %-10s | %s\n",
+                $detail->product->name,
+                $detail->unit_price,
+                $detail->qty,
+                PHP_EOL
+            );
+            $totalProducts += $detail->qty;
+        }
+
+
+        $htmlMessage .= "\n--------------------------------------\n";
+        $htmlMessage .= "ផលិតផល​​​​​​                 | តម្លៃដើម(៛)       |បរិមាណ\n";
+        $htmlMessage .= $productList . "\n";
+        $htmlMessage .= "<b>* សរុបទាំងអស់៖</b> $totalProducts ទំនិញ $orderData->total_price ៛\n";
+        $htmlMessage .= "- កាលបរិច្ឆេទៈ​ ".$orderData->ordered_at;
+
+        TelegramService::sendMessage($htmlMessage);
     }
 }

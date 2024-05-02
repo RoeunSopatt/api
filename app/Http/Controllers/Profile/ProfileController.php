568bd81c318a267e6e59fbf739c5a83e2d0fd6ad
Sopatt Rouern <www.vanhong168@gmail.com>
2024-05-03 00:31:25 +0700

    Create function view in profile

--- app/Http/Controllers/Profile/ProfileController.php
+++ app/Http/Controllers/Profile/ProfileController.php
@@ -3,9 +3,17 @@
 namespace App\Http\Controllers\Profile;
 
 use App\Http\Controllers\Controller;
+use App\Models\User\User;
 use Illuminate\Http\Request;
+use Tymon\JWTAuth\Facades\JWTAuth;
 
 class ProfileController extends Controller
 {
     //
+    public function view(){
+        $auth =JWTAuth::parseToken()->authenticate();
+
+        $user=User::select("id","name","phone","email","avatar")->where('id',$auth->id)->first();
+        return response()->json($user,Response::HTTP_OK);
+    }
 }

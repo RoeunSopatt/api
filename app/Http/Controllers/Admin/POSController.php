18a9074e91e1ddf6fe24ff87f1f70881d0620295
Sopatt Rouern <www.vanhong168@gmail.com>
2024-05-02 20:44:25 +0700

    Create function getProducts in pos

--- app/Http/Controllers/Admin/POSController.php
+++ app/Http/Controllers/Admin/POSController.php
@@ -3,9 +3,17 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Products\Type;
 use Illuminate\Http\Request;
+use ProductType;
 
 class POSController extends Controller
 {
     //
+    public function getProducts(){
+        $products = Type::select("id","name")->with([
+            'products:id,name,image,type_id,unit_price'
+        ])->get();
+        return response()->json($products,Response::HTTP_OK);
+    }
 }

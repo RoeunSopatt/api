94a7a94b8c7970ad8ee7cd2219ba996234a4ed39
Sopatt Rouern <www.vanhong168@gmail.com>
2024-05-03 00:12:13 +0700

    Create public variables and __construct

--- app/Http/Controllers/Admin/PrintController.php
+++ app/Http/Controllers/Admin/PrintController.php
@@ -8,4 +8,14 @@ use Illuminate\Http\Request;
 class PrintController extends Controller
 {
     //
+    public $JS_BASE_URL;
+    public $JS_USERNAME;
+    public $JS_PASSWORD;
+    public $JS_TEMPLATE;
+    public function __construct(){
+        $this->JS_BASE_URL = env('JS_BASE_URL');
+        $this->JS_USERNAME = env('JS_USERNAME');
+        $this->JS_PASSWORD = env('JS_PASSWORD');
+        $this->JS_TEMPLATE = env('JS_TEMPLATE');
+    }
 }

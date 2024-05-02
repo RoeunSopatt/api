0e87e311a71eb695a2ec65e90873f3e12ba3f680
Sopatt Rouern <www.vanhong168@gmail.com>
2024-05-02 23:34:04 +0700

    Create function getData for sale

--- app/Http/Controllers/Admin/SaleController.php
+++ app/Http/Controllers/Admin/SaleController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Orders\Order;
 use Illuminate\Http\Request;
+use Tymon\JWTAuth\Facades\JWTAuth;
 
 class SaleController extends Controller
 {
@@ -14,5 +16,30 @@ class SaleController extends Controller
         }else{
             return true;
         }
+    }
+
+    public function getData(Request $request){
+
+        $data=Order::select('*')->with([
+            'cashier',
+            'details',
+        ]);
+
+        if($request->from && $request->to && $this->_isValidate($request->from)&&$this->_isValidate($request->to)){
+            $data= $data->where('create_at',[$request->from . "00:00:00", $request->to ."23:59:59"]);
+        }
+        if($request->reciept_number){
+            $data->$data->where('receipt_number',$request->reciept_number);
+        }
+        if($request->reciept_number){
+            $data->$data->where('receipt_number',$request->reciept_number);
+        }
+
+        $user =JWTAuth::parseToken()->authenticate();
+        if($user->type_id ==2){
+            $data=$data->where('cashier_id',$user->id);
+        }
+        $data =$data->orderBy('id','desc')->paginate($request->limit ? $request->limit :10);
+        return response()->json($data,Response::HTTP_OK);
     }
 }

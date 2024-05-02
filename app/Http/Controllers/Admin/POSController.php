08c83268edfa9840e6b034e87a3c42338a0ae483
Sopatt Rouern <www.vanhong168@gmail.com>
2024-05-02 21:20:06 +0700

    Create function makeOrder in pos

--- app/Http/Controllers/Admin/POSController.php
+++ app/Http/Controllers/Admin/POSController.php
@@ -4,9 +4,11 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use App\Models\Orders\Order;
+use App\Models\Products\Product;
 use App\Models\Products\Type;
 use App\Services\TelegramService;
 use Illuminate\Http\Request;
+use OrderDetial;
 use ProductType;
 use Tymon\JWTAuth\Facades\JWTAuth;
 
@@ -57,4 +59,85 @@ class POSController extends Controller
 
         TelegramService::sendMessage($htmlMessage);
     }
+
+    public function makeOrder(Request $req){
+
+        // ===>> Check validation
+        $this->validate($req, [
+            'cart'      => 'required|json'
+        ]);
+
+        //==============================>> Get Current Login User to save who make orders.
+        $user = JWTAuth::parseToken()->authenticate();
+
+        // ===>> Create Order
+        $order                  = new Order;
+        $order->cashier_id      = $user->id; // the current login account
+        $order->total_price     = 0; // Set to 0 first. Find total Price later
+        $order->receipt_number  = $this->_generateReceiptNumber(); // return 6 digit unique random invoice number.
+
+        // ===>> Save To DB
+        $order->save();
+
+        // ===>> Find Total Price & Order Detail
+        $details    = [];
+        $totalPrice = 0;
+        $cart       = json_decode($req->cart); // Turn Json String to PHP Array.
+
+        // ===>> Loop Each Product ID along with QTY
+        foreach ($cart as $productId => $qty) {
+
+            // ===>> Find Each Product by ID
+            $product = Product::find($productId);
+
+            // ===>> Check if product is valid
+            if ($product) { // Yes
+
+                // ===>> Add New Record to Array
+                $details[] = [
+                    'order_id'      => $order->id,
+                    'product_id'    => $productId,
+                    'qty'           => $qty,
+                    'unit_price'    => $product->unit_price,
+                ];
+
+                // ===>> Calculate the total Price
+                $totalPrice +=  $qty * $product->unit_price;
+
+            }
+        }
+
+        // ===>> Save Order Detail to DB
+        OrderDetial::insert($details);
+
+        // ===>> Update Order
+        $order->total_price     = $totalPrice;
+        $order->ordered_at      = Date('Y-m-d H:i:s');
+        $order->save();
+
+        // ===> Get Data for Client Reponse to view the order in Popup.
+        $orderData = Order::select('*')
+        ->with([
+
+            'cashier:id,name,type_id', // M:1
+            'cashier.type:id,name',  // M:1
+
+            'details:id,order_id,product_id,unit_price,qty', // 1:M
+            'details.product:id,name,type_id', // M:1 (order_details -> product)
+            'details.product.type:id,name'  // M:1 (product -> products_type)
+
+        ])
+        ->find($order->id);
+
+        // ===>> Send Telegram Notification
+        $this->_sendNotification($orderData);
+
+        // ===> Success Response Back to Client
+        return response()->json([
+            'order'         => $orderData,
+            'message'       => 'ការបញ្ជាទិញត្រូវបានបង្កើតដោយជោគជ័យ។'
+        ], Response::HTTP_OK);
+
+    }
+
 }

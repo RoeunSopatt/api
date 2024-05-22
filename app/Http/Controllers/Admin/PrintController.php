169466c3b220ce079337ca56317fe28c26a22c8d
Sopatt Rouern <www.vanhong168@gmail.com>
2024-05-22 10:14:48 +0700

    fix PrintController with JsReport

--- app/Http/Controllers/Admin/PrintController.php
+++ app/Http/Controllers/Admin/PrintController.php
@@ -1,57 +1,66 @@
 <?php
 
 namespace App\Http\Controllers\Admin;
-
-
+use Illuminate\Support\Facades\Http;
 use App\Http\Controllers\MainController;
 use App\Models\Orders\Order;
-use Http;
-use Illuminate\Http\Request;
+use Illuminate\Database\QueryException;
 
 class PrintController extends MainController
 {
-    //
-    public $JS_BASE_URL;
-    public $JS_USERNAME;
-    public $JS_PASSWORD;
-    public $JS_TEMPLATE;
-    public function __construct(){
-        $this->JS_BASE_URL = env('JS_BASE_URL');
-        $this->JS_USERNAME = env('JS_USERNAME');
-        $this->JS_PASSWORD = env('JS_PASSWORD');
-        $this->JS_TEMPLATE = env('JS_TEMPLATE');
+    private $JS_BASE_URL;
+    private $JS_USERNAME;
+    private $JS_PASSWORD;
+    private $JS_TEMPLATE;
+
+    public function __construct()
+    {
+        $this->JS_BASE_URL   = env('JS_BASE_URL');
+        $this->JS_USERNAME   = env('JS_USERNAME');
+        $this->JS_PASSWORD   = env('JS_PASSWORD');
+        $this->JS_TEMPLATE   = env('JS_TEMPLATE');
     }
-    public function printInvioceOrder($receiptNumber=0){
+
+    public function printInvoiceOrder($receiptNumber = 0)
+    {
         try {
-            //code...
-            $url =$this->JS_BASE_URL."/api/report";
+
+            // URL Preparation
+
+            $url = "https://sopatt123.jsreportonline.net/api/report";
+            // Debugging: Log the constructed URL
             info("Constructed URL: $url");
-            $receipt = Order::select('id','receipt_number','cashier_id','total_price','ordered_at')
-            ->with([
-                'cashier',
-                'details'
-            ])
-            ->where('receipt_number',$receiptNumber)
-            ->orderBy('id','desc')
-            ->get();
-            $totalPrice = 0;
-            foreach ($receipt as $row) {
-                $totalPrice += $row->total_price;
-            }
+
+            // Get Data from DB
+            $receipt = Order::select('id', 'receipt_number', 'cashier_id', 'total_price', 'ordered_at')
+                ->with([
+                    'cashier', // M:1
+                    'details' // 1:M
+                ])
+                ->where('receipt_number', $receiptNumber)
+                // ->orderBy('id', 'desc')
+                ->first();
+
+            // Find Total Price
+            // $totalPrice = 0;
+            // foreach ($receipt as $row) {
+            //     $totalPrice += $row->total_price;
+            // }
 
             // Prepare Payload for JS Report Service
             $payload = [
                 "template" => [
-                    "name" => $this->JS_TEMPLATE,
+                    "name" => 'main',
                 ],
-                "data" => [
-                    'total' => $totalPrice,
-                    'data'  => $receipt,
-                ],
+                // "data" => [
+                //     'total' => $totalPrice,
+                //     'data'  => $receipt,
+                // ],
+                'data'  => $receipt,
             ];
 
             // Send Request to JS Report Service
-            $response = Http::withBasicAuth($this->JS_USERNAME, $this->JS_PASSWORD)
+            $response = Http::withBasicAuth('dopaminegm@mail.com','Patt2004')
                 ->withHeaders([
                     'Content-Type' => 'application/json',
                 ])
@@ -62,12 +71,13 @@ class PrintController extends MainController
                 'file_base64'   => base64_encode($response),
                 'error'         => '',
             ];
-        } catch (\Throwable $th) {
-            //throw $th;
+        } catch (\Exception $e) {
+            // Handle the exception
             return [
                 'file_base64' => '',
-                'error' => $th->getMessage(),
+                'error' => $e->getMessage(),
             ];
         }
     }
+
 }
--- routes/api/test.php
+++ routes/api/test.php
@@ -4,13 +4,14 @@
 use App\Services\FileUpload;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Testing\EmailController;
+use App\Services\TelegramService;
 
 // ========================================================================>> Send Email
 Route::post('/send-email', [EmailController::class, 'sendEmailRaw']);
 
 // ========================================================================>> Telegram Bot
 
-
+Route::get('/send-telegram-bot',[TelegramService::class,'sendMessage']);
 // ========================================================================>> JSReport
 
 
